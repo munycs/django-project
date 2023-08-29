6cc24d81baabef26a59d0be1b0f1fe124ad931e9
munycs <131538877+munycs@users.noreply.github.com>
2023-08-29 11:20:29 +0700

    Update form.py

--- feed/form.py
+++ feed/form.py
@@ -3,5 +3,5 @@ from django import forms
 class PostForm(forms.Form):
    image = forms.ImageField()
    text = forms.CharField(label="Title")
-   description = forms.CharField(label="Description")
+   #description = forms.CharField(label="Description")
     
