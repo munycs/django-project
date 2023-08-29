b226a27b2d9aba89452d4f79a23f9d9e6693a889
munycs <131538877+munycs@users.noreply.github.com>
2023-08-29 11:12:58 +0700

    Add files via upload

---
+++ feed/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from sorl.thumbnail import ImageField
+# Create your models here.
+class Post(models.Model):
+    text = models.CharField(max_length=140, blank=False, null=False)
+    image = ImageField()
+    des = models.CharField(max_length=5000, blank=False, null=False)
+    def __str__(self):
+        return self.text
+
